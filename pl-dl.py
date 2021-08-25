dc6dea61d94d6ad0c19b9fb808cccf4478b8deae
ladm99 <77761706+ladm99@users.noreply.github.com>
2021-08-25 09:39:52 -0500

    Fixed ffmpeg issue
    Fixed ffpmeg issue where the playlist script would not work if ffmpeg was not added to the PATH

--- pl-dl.py
+++ pl-dl.py
@@ -10,20 +10,10 @@ def main():
 	while True:
 
 		if path.exists('config.pkl') == False:
-			resolution = input('Desired resolution of the videos you want to download (144p, 360p, 720p, 1080p, etc.): ')
-			x = input('Do you want to download captions [Y,N]: ')
-			if x == 'y' or x == 'Y':
-				cap = '1'
-			else:
-				cap = '2'
-			lang_code = input('Enter languge code of the captions you wish to download (en for english): ')
-
-			output = open('config.pkl', 'wb')
-			config = Config(resolution, cap, lang_code)
-			pickle.dump(config, output, -1)
-			output.close()
+			createConfig()
 		else:
-			print('\n1. Enter url\n2. Edit config\n3. Quit')
+			print('Enter Selection\n' + '---------------')
+			print('1. Enter url\n2. Edit config\n3. Quit')
 			select = input('Enter: ')
 			if select == '1':
 				url = ''
@@ -31,18 +21,7 @@ def main():
 				playlist = Playlist(url)
 				pl(playlist)
 			elif select == '2':
-				resolution = input('Desired resolution of the videos you want to download (144p, 360p, 720p, 1080p, etc.): ')
-				x = input('Do you want to download captions [Y,N]: ')
-				if x == 'y' or x == 'Y':
-					cap = '1'
-				else:
-					cap = '2'
-				lang_code = input('Enter languge code of the captions you wish to download (en for english): ')
-
-				output = open('config.pkl', 'wb')
-				config = Config(resolution, cap, lang_code)
-				pickle.dump(config, output, -1)
-				output.close()
+				createConfig()
 			elif select == '3':
 				exit()
 
@@ -58,25 +37,27 @@ def adapt(yt):
 	config = pickle.load(data)
 
 	video = yt.streams.filter(resolution = config.resolution, only_video = True, adaptive=True, file_extension='mp4')
-	audio = yt.streams.filter(only_audio=True,file_extension='mp4')
+	audio = yt.streams.filter(only_audio=True,file_extension='mp4').order_by('abr').desc()
+	#puts audio stream with highest bitrate at index 0
 
-	if len(video) != 0:
+	if len(video) != 0 and len(audio):
 		print('Downloading ' + fix_text(yt.title) + ' video | ' + str(convertToMegs(video[0].filesize)) + 'MB')
-		video[0].download(filename=fix_text(yt.title))		
+		video[0].download(filename=fix_text(yt.title) + '.mp4')		
 		print('Video download finished')
 
 		print('Downloading ' + fix_text(yt.title) + ' audio | ' + str(convertToMegs(audio[0].filesize)) + 'MB')
-		audio[0].download(filename=(fix_text(yt.title) + ' audio'))
+		audio[0].download(filename=(fix_text(yt.title) + ' audio' + '.mp4'))
 		print('Audio download finished')
 	else:
-		print('Could not find the video in the resolution you were looking for')
-		video = yt.streams.filter(only_video = True, adaptive=True, file_extension='mp4')
-
-		count = 0
+		print('Could not find the video in the resolution you were looking for, downlading the next highest resolution avaliable')
+		video = yt.streams.filter(only_video = True, adaptive=True, file_extension='mp4').order_by('resolution').desc()
+		#orders the video streams by resolution in descencing order so that the highest resolution is at index 0
+		
+		"""count = 0
 		print('Video: ' + fix_text(yt.title))
 		for x in video:
 			print(str(count) + ': ',end='')
-			print(x)
+			printStream(x)
 			print()
 			count+=1
 		vIndex = input('Select which video to download: \n')
@@ -86,18 +67,18 @@ def adapt(yt):
 		print('Audio: ' + fix_text(yt.title))
 		for x in audio:
 			print(str(count) + ': ',end='')
-			print(x)
+			printStream(x)
 			print()
-			count+=1
+			count+=1"
 		aIndex = input('Select which Audio to download: ')
-		aIndex = int(aIndex)
-		print('Downloading ' + fix_text(yt.title) + ' video | ' + str(convertToMegs(video[vIndex].filesize)) + 'MB')
-		video[vIndex].download(filename=fix_text(yt.title))
+		aIndex = int(aIndex)"""
+		print('Downloading ' + fix_text(yt.title) + ' video | ' + str(convertToMegs(video[0].filesize)) + 'MB')
+		video[0].download(filename=fix_text(yt.title) + '.mp4')
 		print('Video download finished')
-		out = yt.title + ' audio'
+		out = yt.title + ' audio' + '.mp4'
 		out = fix_text(out)
-		print('Downloading ' + fix_text(yt.title) + ' audio | ' + str(convertToMegs(audio[aIndex].filesize)) + 'MB')
-		audio[aIndex].download(filename=out)
+		print('Downloading ' + fix_text(yt.title) + ' audio | ' + str(convertToMegs(audio[0].filesize)) + 'MB')
+		audio[0].download(filename=out)
 		print('Audio download finished')
 
 #displays and downloads the captions (srt file)
@@ -162,7 +143,7 @@ def mux(yt,prog, cap_true):
 #if the video is adaptive and captions were downloaded
 	if prog == False and cap_true == '1':
 		
-		cmd = 'ffmpeg -i ' + video + ' -i ' + audio + ' -i ' + subs + ' -c copy ' + output
+		cmd = '..\\ffmpeg -i ' + video + ' -i ' + audio + ' -i ' + subs + ' -c copy ' + output
 
 		os.system(cmd)
 		delete = 'del ' + video + ' ' + audio + ' ' + subs
@@ -171,7 +152,7 @@ def mux(yt,prog, cap_true):
 #if the video is adaptive and captions were not downloaded
 	elif prog == False and cap_true == '2':
 
-		cmd = 'ffmpeg -i ' + video + ' -i ' + audio +  ' -c copy ' + output
+		cmd = '..\\ffmpeg -i ' + video + ' -i ' + audio +  ' -c copy ' + output
 		os.system(cmd)
 		delete = 'del ' + video + ' ' + audio
 		os.system(delete)
@@ -179,7 +160,7 @@ def mux(yt,prog, cap_true):
 #if the video is progressive and captions were downloaded
 	elif prog == True and cap_true == '1':
 
-		cmd = 'ffmpeg -i ' + video + ' -i ' + subs + ' -c copy ' + output
+		cmd = '..\\ffmpeg -i ' + video + ' -i ' + subs + ' -c copy ' + output
 
 		os.system(cmd)
 		delete = 'del ' + video + ' ' + subs
@@ -194,7 +175,7 @@ def pl(playlist):
 	p = playlist
 	title = fix_text(p.title)
 
-	print('Playlist found - ' + title)
+	print('\nPlaylist found - ' + title)
 	try:
 		os.mkdir(title)
 	except OSError as error:
@@ -202,7 +183,7 @@ def pl(playlist):
 	os.chdir(title)
 
 	for url in p.video_urls:
-		print('\n')
+		#print('\n')
 		yt = YouTube(url, on_progress_callback=on_progress)
 		adapt(yt)
 		cap_true = cap(yt)
@@ -231,5 +212,32 @@ def fix_text(text):
 #conversion function
 def convertToMegs(num):
 	return round(num / 1000000, 2)
+
+def printStream(stream):
+	audio = stream.includes_audio_track
+	video = stream.includes_video_track
+	adapt = stream.is_adaptive
+	if adapt == True:
+		if video == True:
+			print(f'Type: {stream.mime_type}, Resolution: {stream.resolution}, fps: {stream.fps}, Video Codec: {stream.video_codec}, Size: {convertToMegs(stream.filesize)}MB')
+		else:
+			print(f'Type: {stream.mime_type}, Bitrate: {stream.abr}, Audio Codec: {stream.audio_codec}, Size: {convertToMegs(stream.filesize)}MB')
+	else:
+		print(f'Type: {stream.mime_type}, Resolution: {stream.resolution}, fps: {stream.fps}, Video Codec: {stream.video_codec}, Audio Codec: {stream.audio_codec}, Size: {convertToMegs(stream.filesize)}MB')
+
+
+def createConfig():
+	resolution = input('Desired resolution of the videos you want to download (144p, 360p, 720p, 1080p, etc.): ')
+	x = input('Do you want to download captions [Y,N]: ').lower()
+	if x == 'y':
+		cap = '1'
+	else:
+		cap = '2'
+	lang_code = input('Enter languge code of the captions you wish to download (en for english): ').lower()
+
+	output = open('config.pkl', 'wb')
+	config = Config(resolution, cap, lang_code)
+	pickle.dump(config, output, -1)
+	output.close()
 
 main()
