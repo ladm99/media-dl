366147c8b9338ea22b717a8ba6e251413baa1b65
ladm99 <ladelmar99@gmail.com>
2022-05-19 01:32:45 -0500

    added buildCommand method

--- cmdBuilder.py
+++ cmdBuilder.py
@@ -1,10 +1,22 @@
 class cmdBuilder(object):
-	"""docstring for cmdBuilder"""
-	def __init__(self, url):
+	"""
+	docstring for cmdBuilder
+	Builds the command prompt commands 
+	options are stored in a list	
+	"""
+	def __init__(self, url, options):
 		super(cmdBuilder, self).__init__()
 		self.url = url
-		options = []
+		self.options = options
 
-	def addOption(option):
-		options.append(option)
+	def addOption(self, option):
+		self.options.append(' ' + option)
+
+	def buildCommand(self):
+		out = 'yt-dlp.exe'
+
+		for x in self.options:
+			out = out + x
+		out = out + ' ' +self.url
+		return out
 		
