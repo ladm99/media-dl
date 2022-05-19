5d230a9935f935296d54c9686b0c4bf1c756233c
ladm99 <ladelmar99@gmail.com>
2022-05-19 01:33:09 -0500

    changed cap to subs

--- config.py
+++ config.py
@@ -3,21 +3,21 @@
 import _pickle as pickle
 
 class Config(object):
-	def __init__(self, resolution, cap, lang_code):
+	def __init__(self, resolution, subs, lang_code):
 		self.resolution = resolution
-		self.cap = cap
+		self.subs = subs
 		self.lang_code = lang_code
 
 	def createConfig():
-		resolution = input('Desired resolution of the videos you want to download (144p, 360p, 720p, 1080p, etc.): ')
-		x = input('Do you want to download captions [Y,N]: ').lower()
+		resolution = input('Desired resolution of the videos you want to download (144, 360, 720, 1080, etc.): ')
+		x = input('Do you want to download subtitles [Y,N]: ').lower()
 		if x == 'y':
-			cap = '1'
+			subs = '1'
 		else:
-			cap = '2'
-		lang_code = input('Enter languge code of the captions you wish to download (en for english): ').lower()
+			subs = '2'
+		lang_code = input('Enter languge code of the subtitles you wish to download (en for english): ').lower()
 
 		output = open('config.pkl', 'wb')
-		config = Config(resolution, cap, lang_code)
+		config = Config(resolution, subs, lang_code)
 		pickle.dump(config, output, -1)
 		output.close()
