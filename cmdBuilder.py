b247335d86c1165116300d0c517247b299d45d3e
ladm99 <ladelmar99@gmail.com>
2022-05-18 23:44:10 -0500

    added cmdBuilder

---
+++ cmdBuilder.py
@@ -0,0 +1,10 @@
+class cmdBuilder(object):
+	"""docstring for cmdBuilder"""
+	def __init__(self, url):
+		super(cmdBuilder, self).__init__()
+		self.url = url
+		options = []
+
+	def addOption(option):
+		options.append(option)
+		
